2a95d93eedd790bd5851c2e6ba15ecb644704656
DOOLEY NSEWOLO <dooleynsewolo@MacBook-Pro-de-DOOLEY.local>
2013-05-14 00:17:36 +0200

    bug session storage fixed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-StudioSabab::Application.config.session_store :encrypted_cookie_store, key: '_studio_sabab_session'
+#StudioSabab::Application.config.session_store :encrypted_cookie_store, key: '_studio_sabab_session'
+StudioSabab::Application.config.session_store :cookie_store, key: '_studio_sabab_session'
