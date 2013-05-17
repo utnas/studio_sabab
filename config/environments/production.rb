eb0386b4c8d18b7f3ebbc273d0425dd96d508657
DOOLEY NSEWOLO <dooleynsewolo@MacBook-Pro-de-DOOLEY.local>
2013-05-17 15:31:49 +0200

    config bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 StudioSabab::Application.configure do
-  config.ember.variant = :production
+  #config.ember.variant = :production
 
   # Settings specified here will take precedence over those in config/application.rb.
 
